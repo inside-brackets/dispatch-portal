f7c30284587072a15c0e75314c47333eae8bc13d
ahmed-khalid9199 <ahmedkhalid9199@gmail.com>
2021-11-02 14:16:14 +0500

    proxy testing

--- server/server.js
+++ server/server.js
@@ -88,27 +88,20 @@ io.on("connection", (socket) => {
 
 // redirecting
 app.all("*", (req, res, next) => {
-  // var ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-  var ip_info = get_ip(req);
-  // const ipList = getIpList();
-  console.log("req.socket.remoteAddress", req.socket.remoteAddress);
-  console.log("req.connection.remoteaddress", req.connection.remoteaddress);
-  console.log('req.headers["x-forwarded-for"]', req.headers["x-forwarded-for"]);
-  console.log('req.headers["x-real-ip"]', req.headers["X-Real-IP"]);
-  console.log("ip_info.clientIp", ip_info.clientIp);
-  console.log("req.ip", req.ip);
-  console.log("client_ip", req.headers["client_ip"]);
-  next();
-  // if (req.url.includes("whitelist")) {
-  //   next();
-  // } else if (ipList.includes(ip.replace("::ffff:", "").trim())) {
-  //   next();
-  // } else {
-  //   io.sockets.emit("not-listed", "logout");
-  //   res.status(401).send({
-  //     message: "not white listed",
-  //   });
-  // }
+  var ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  ip = ip.replace("::ffff:", "").trim();
+  console.log("x-forwarded-for", ip);
+  const ipList = getIpList();
+  if (req.url.includes("whitelist")) {
+    next();
+  } else if (ipList.includes(ip.replace("::ffff:", "").trim())) {
+    next();
+  } else {
+    io.sockets.emit("not-listed", "logout");
+    res.status(401).send({
+      message: "not white listed",
+    });
+  }
 });
 app.post("/whitelist/:mac/:ip", (req, res) => {
   setIp(req.params.mac, req.params.ip);
