2ca27f537dd1c1e18e3ce88470fde9d211ea06a0
Faraz Mahmood <78564905+Faraz-Mahmood98@users.noreply.github.com>
2022-04-07 00:37:58 +0500

    graphs addded

--- client/src/pages/admin/UserDetail.jsx
+++ client/src/pages/admin/UserDetail.jsx
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { Row, Col, Card, Form } from "react-bootstrap";
+import { Row, Col, Card, Form, Alert } from "react-bootstrap";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import Loader from "react-loader-spinner";
 import moment from "moment";
-import { PieChart, Pie, Cell, Tooltip,ResponsiveContainer } from "recharts";
+import { PieChart, Pie, Cell, Tooltip ,  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Legend } from "recharts";
 
 const UserDetail = () => {
   const params = useParams();
   const [user, setUser] = useState(null);
   const [data, setData] = useState(null);
+  const [message, setMessage] = useState(`Loading`);
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_BACKEND_URL}/getuser/${params.id}`)
@@ -33,13 +39,65 @@ const UserDetail = () => {
         const registered = res.data.filter(
           (carrier) => carrier.change === "registered"
         );
-        setData([
-          { name: "Registered", value: registered.length },
-          { name: "Rejected", value: rejected.length },
-        ]);
+
+        if (rejected.length === 0 && registered.length === 0) {
+          setMessage(`Not enough data to show in graph`);
+        } else {
+          setData([
+            { name: "Registered", value: registered.length },
+            { name: "Rejected", value: rejected.length },
+          ]);
+        }
       })
       .catch((err) => console.log(err));
+
+
   }, [params.id]);
+
+  const data01 = [
+    {
+      name: "month",
+      salary: 4000,
+      dispactherFee: 2400,
+    },
+    {
+      name: "month",
+      salary: 3000,
+      dispactherFee: 1398,
+      amt: 2210
+    },
+    {
+      name: "Page C",
+      salary: 2000,
+      dispactherFee: 9800,
+      amt: 2290
+    },
+    {
+      name: "Page D",
+      salary: 2780,
+      dispactherFee: 3908,
+      amt: 2000
+    },
+    {
+      name: "Page E",
+      salary: 1890,
+      dispactherFee: 4800,
+      amt: 2181
+    },
+    {
+      name: "Page F",
+      salary: 2390,
+      dispactherFee: 3800,
+      amt: 2500
+    },
+    {
+      name: "Page G",
+      salary: 3490,
+      dispactherFee: 4300,
+      amt: 2100
+    }
+  ];
+  
 
   const COLORS = ["#0088FE", "#00C49F"];
 
@@ -71,7 +129,7 @@ const UserDetail = () => {
   };
 
   return (
-    <Row>
+    <Row className="justify-content-center">
       {!user ? (
         <Loader type="MutatingDots" color="#349eff" height={100} width={100} />
       ) : (
@@ -79,7 +137,7 @@ const UserDetail = () => {
           <Card>
             <Form>
               <Row className="m-3">
-                <h1 className="text-center">User Detail Page</h1>
+                <h1 className="text-center">User Detail</h1>
                 <Form.Group as={Col} md="6">
                   <Form.Label>User Name</Form.Label>
                   <Form.Control
@@ -201,32 +259,60 @@ const UserDetail = () => {
                 </Form.Group>
               </Row>
             </Form>
+          </Card>
+          <Card>
             <Row>
-              {!data ? (
-                <p>Loadingg</p>
-              ) : (
-                // <ResponsiveContainer width="100%" height="100%">
-                  <PieChart width={400} height={400}>
-                    <Pie
-                      data={data}
-                      cx="50%"
-                      cy="50%"
-                      labelLine={false}
-                      label={renderCustomizedLabel}
-                      outerRadius={80}
-                      fill="#8884d8"
-                      dataKey="value"
-                    >
-                      {data.map((entry, index) => (
-                        <Cell
-                          key={`cell-${index}`}
-                          fill={COLORS[index % COLORS.length]}
-                        />
-                      ))}
-                    </Pie>
-                  </PieChart>
-// {/* </ResponsiveContainer> */}
-              )}
+              <h1 className="text-center">Stats</h1>
+              
+                  <Col>
+                  <h3>Monthly Dialing report</h3>
+                  {!data ?           <Alert variant="primary">{message}</Alert>
+:  
+                    <PieChart width={400} height={400}>
+                      <Pie
+                        data={data}
+                        cx="50%"
+                        cy="50%"
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius={80}
+                        fill="#8884d8"
+                        dataKey="value"
+                      >
+                        {data.map((entry, index) => (
+                          <Cell
+                            key={`cell-${index}`}
+                            fill={COLORS[index % COLORS.length]}
+                          />
+                        ))}
+                      </Pie>
+                      <Tooltip />
+                    </PieChart>
+}
+   </Col>
+                  <Col>
+                  <h3>Profit Loss Analysis</h3>
+                  <BarChart
+      width={500}
+      height={300}
+      data={data01}
+      margin={{
+        top: 20,
+        right: 30,
+        left: 20,
+        bottom: 5
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="dispactherFee" stackId="a" fill="#8884d8" />
+      <Bar dataKey="salary" stackId="a" fill="#82ca9d" />
+    </BarChart>
+                  </Col>
+            
             </Row>
           </Card>
         </Col>
