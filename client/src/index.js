353636894ac40dd1b58492e8f1b555ab6bc01052
ahmed-khalid9199 <ahmedkhalid9199@gmail.com>
2022-04-09 23:27:41 +0500

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,6 @@ import "./assets/boxicons-2.0.7/css/boxicons.min.css";
 import "./assets/css/grid.css";
 import "./assets/css/theme.css";
 import "./assets/css/index.css";
-import "tippy.js/dist/tippy.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import { io } from "socket.io-client";
