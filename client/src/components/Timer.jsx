6d277ce220cdbabd395498165895351d705102ed
farazirfan12 <74297513+Ahmed-khalid9199@users.noreply.github.com>
2022-04-10 17:02:14 +0500

    testing by faraz

--- client/src/components/Timer.jsx
+++ client/src/components/Timer.jsx
@@ -6,21 +6,6 @@ import { useSelector } from "react-redux";
 const Timer = () => {
   const { user } = useSelector((state) => state.user);
   useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_BACKEND_URL}/sales/get-closet/${user?._id}`)
-      .then((res) => {
-        console.log(res.data);
-        setTimeout(() => {
-          axios
-            .post(`${process.env.REACT_APP_BACKEND_URL}/notification`, {
-              user: res.data[0].salesman,
-              msg: `Your Appointment with ${res.data[0].mc_number} is started now`,
-            })
-            .then((res) => toast.success("Your meeting starts now"))
-            .catch((err) => console.log(err));
-        }, res.data[0].difference);
-      })
-      .catch((err) => console.log(err));
     let counter = JSON.parse(localStorage.getItem("counters"));
     const date = new Date().setHours(17, 0, 0);
 
@@ -43,6 +28,23 @@ const Timer = () => {
           date,
         })
       );
+      axios
+        .get(
+          `${process.env.REACT_APP_BACKEND_URL}/sales/get-closet/${user?._id}`
+        )
+        .then((res) => {
+          console.log(res.data);
+          setTimeout(() => {
+            axios
+              .post(`${process.env.REACT_APP_BACKEND_URL}/notification`, {
+                user: res.data[0].salesman,
+                msg: `Your Appointment with ${res.data[0].mc_number} is started now`,
+              })
+              .then((res) => toast.success("Your meeting starts now"))
+              .catch((err) => console.log(err));
+          }, res.data[0].difference);
+        })
+        .catch((err) => console.log(err));
     }, timeOut);
     return () => clearTimeout(timer);
   }, []);
