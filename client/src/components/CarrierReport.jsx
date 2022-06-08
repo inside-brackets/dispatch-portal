87f92d0b34fdc628464073510c8735317dc70d33
Faraz Mahmood <78564905+Faraz-Mahmood98@users.noreply.github.com>
2022-06-08 22:56:43 +0500

    chnages inn df

--- client/src/components/CarrierReport.jsx
+++ client/src/components/CarrierReport.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useCallback } from "react";
 import { Col, Row, Button } from "react-bootstrap";
 import moment from "moment";
 import axios from "axios";
@@ -34,6 +34,18 @@ const CarrierReport = ({
       ? defaultValue.manager_comment && defaultValue.manager_comment
       : null
   );
+  const [deadHeadValue, setDeadHeadValue] = useState(
+    deadHead.reduce((previousValue, currentValue) => {
+      if (currentValue.distance) {
+        return (
+          previousValue +
+          parseFloat(
+            currentValue.distance.text.split(" ")[0].replaceAll(",", "")
+          )
+        );
+      } else return previousValue + 0;
+    }, 0)
+  );
 
   const loadedMiles = load
     .filter((l) => l.l_status === "delivered")
@@ -46,14 +58,6 @@ const CarrierReport = ({
     0
   );
 
-  const deadHeadMiles = deadHead.reduce((previousValue, currentValue) => {
-    if (currentValue.distance) {
-      return (
-        previousValue +
-        parseFloat(currentValue.distance.text.split(" ")[0].replaceAll(",", ""))
-      );
-    } else return previousValue + 0;
-  }, 0);
   let dollarPerLoadedMiles;
   if (amounts === 0 || loadedMiles === 0) {
     dollarPerLoadedMiles = 0;
@@ -61,8 +65,8 @@ const CarrierReport = ({
     dollarPerLoadedMiles = amounts / loadedMiles;
   }
 
-  const totalMiles = loadedMiles + deadHeadMiles;
-  const totalGross = 12323;
+  const totalMiles = loadedMiles + parseInt(deadHeadValue);
+  const totalGross = load.reduce((pre, curr) => pre + curr.pay, 0);
   let dollarPerTotalMiles;
   if (amounts === 0 || loadedMiles === 0) {
     dollarPerTotalMiles = 0;
@@ -96,6 +100,7 @@ const CarrierReport = ({
           loads: load.map((l) => l._id),
           deadHead: deadHead,
           dispatcher: dispatcher._id,
+          manager_comment: managerComments,
         }
       );
     }
@@ -103,11 +108,7 @@ const CarrierReport = ({
   };
   return (
     <>
-      <Row
-        id="div-to-print"
-        ref={ref}
-        className="justify-content-around m-3"
-      >
+      <Row id="div-to-print" ref={ref} className="justify-content-around m-3">
         <h3 className="text-center main-heading">{carrier?.company_name}</h3>
         <h5 className="text-center mt-3">({carrier?.mc_number})</h5>
         <h4 className="text-center mt-3 second-heading">Carrier Report</h4>
@@ -118,7 +119,7 @@ const CarrierReport = ({
           ).format("MMMM D, YYYY")}`}
           )
         </h5>
-        <br/>
+        <br />
         <h1 className="text-center my-4">Statistics</h1>
         <table>
           <tr>
@@ -150,50 +151,62 @@ const CarrierReport = ({
             </td>
           </tr>
         </table>
-        <Row className='my-5'>
-            <h1 className="text-center mt-2">Loaded Miles Vs Deadhead</h1>
+        <Row className="my-4">
+          <h1 className="text-center mt-2">Loaded Miles Vs Deadhead</h1>
           <Col>
-
             <Graphs
               type="donut"
               loadedMiles={loadedMiles}
-              deadHeadMiles={deadHeadMiles}
+              deadHeadMiles={parseInt(deadHeadValue)}
             />
           </Col>
-          <Col style={{
-            marginTop:"150px"
-          }}>
+          <Col
+            style={{
+              marginTop: "150px",
+            }}
+          >
             <Row>
               <Col className="loaded-miles my-3">
-                Loaded Miles: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {loadedMiles}
+                Loaded Miles: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{" "}
+                {loadedMiles}
               </Col>
             </Row>
             <Row>
               <Col className="deadhead-miles">
-                DeadHead Miles: &nbsp;&nbsp;&nbsp;&nbsp;{deadHeadMiles}
+                <Row>
+                  <Col md={5}> DeadHead Miles:</Col>
+                  <Col md={5}>
+                    <input
+                      type="number"
+                      className="form-control"
+                      value={deadHeadValue}
+                      onChange={(e) => setDeadHeadValue(e.target.value)}
+                    />
+                  </Col>
+                </Row>
               </Col>
             </Row>
           </Col>
         </Row>
-        <table>
-          <tr>
-            <td className="major-details">Dispatcher Remarks:</td>
-          </tr>
-          <tr>
-            <td>
-              <textarea
-                value={dispatcherComments}
-                // disabled={defaultValue}
-                onChange={(e) => setDispatcherComments(e.target.value)}
-                className="form-group border text-area2"
-              />
-            </td>
-          </tr>
-        </table>
-
-        <h2 style={{
-          marginTop:'180px'
-        }} className="text-center">
+        <Row>
+          <Col className="major-details">Dispatcher Remarks:</Col>
+        </Row>
+        <Row>
+          <Col>
+            <textarea
+              value={dispatcherComments}
+              // disabled={defaultValue}
+              onChange={(e) => setDispatcherComments(e.target.value)}
+              className="form-group border text-area"
+            />
+          </Col>
+        </Row>
+        <h2
+          style={{
+            marginTop: "170px",
+          }}
+          className="text-center"
+        >
           Have control of your business with our Statistical Analysis
         </h2>
         <Row className="justify-content-center mt-5">
@@ -208,7 +221,7 @@ const CarrierReport = ({
 
         <h3
           style={{
-            marginTop: "150px",
+            marginTop: "320px",
           }}
           className="text-center"
         >
