b7c639d649f400036938b8e7cec987c0d55c001b
ahmedkhalid9199 <ahmedkhalid9199@gmail.com>
2022-08-16 21:52:52 +0500

    interview modal

--- server/models/interview.js
+++ server/models/interview.js
@@ -1,40 +1,17 @@
 const mongoose = require("mongoose");
+
 const interviewSchema = new mongoose.Schema(
   {
     candidate: {
-      first_name: {
-        type: String,
-      },
-      last_name: {
-        type: String,
-      },
-      date_of_birth: {
-        type: Date,
-      },
-      salary: {
-        type: Number,
-        require: true,
-        trim: true,
-      },
-
-      phone_number: {
-        type: String,
-        trim: true,
-      },
-      email_address: {
-        type: String,
-      },
-      address: {
-        type: String,
-      },
-      designation: {
-        type: String,
-        default: "employee",
-      },
+      first_name: String,
+      last_name: String,
+      phone_number: String,
+      address: String,
+      designation: String,
       department: {
         type: String,
         enum: ["sales", "dispatch", "HR", "admin", "accounts", "undefined"],
-        required: true,
+        default: "undefined",
       },
       company: {
         type: String,
@@ -42,13 +19,16 @@ const interviewSchema = new mongoose.Schema(
         required: true,
       },
     },
+    status: {
+      type: String,
+      enum: ["scheduled", "rejected", "hired", "pending-decision"],
+      default: "scheduled",
+    },
+    remarks: String,
     interviewer: {
       type: mongoose.Types.ObjectId,
       ref: "User",
-      required: true,
     },
-    remarks: String,
-    date_time: Date,
   },
   {
     timestamps: true,
