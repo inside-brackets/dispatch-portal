ea5e5d8154e4464c58c9151bf89647bef6d78548
Haris S <113344267+ItsMe-Haris@users.noreply.github.com>
2022-11-14 20:18:31 +0500

    Task 5
    Implement API for salary table and populate table.

--- server/routes/salary.js
+++ server/routes/salary.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { updateSlots } = require("../controllers/salary");
+const { updateSlots, getSalaries } = require("../controllers/salary");
 
 const router = express.Router();
 
 router.post("/update/slots/:id", updateSlots);
+router.post("/get/salaries", getSalaries);
 
 module.exports = router;
