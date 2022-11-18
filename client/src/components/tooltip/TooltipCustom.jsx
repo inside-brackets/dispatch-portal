84713da91cf732bdca92ce5d4ba85afc41408126
ABDUL REHMAN <abdulrehman499949@gmail.com>
2022-11-18 21:08:56 +0500

    File Handling Completed

--- client/src/components/tooltip/TooltipCustom.jsx
+++ client/src/components/tooltip/TooltipCustom.jsx
@@ -4,7 +4,7 @@ import ReactTooltip from "react-tooltip";
 import './tooltip.css'
 const tooltip = (props) => {
   return (<>
-    <ReactTooltip id={`${props.id}`} place="top" effect="solid" className="tooltips" arrowColor="#349eff" style={{backgroundColor:"#fa1b23 "}} offset={{top:props.top || 1, left: props.left || 0,right:props.right || 0}}>
+    <ReactTooltip id={`${props.id}`} place="top" effect="solid" className="tooltips" arrowColor="none" style={{backgroundColor:"#fa1b23 "}} offset={{top:props.top || 0, left: props.left || 0,right:props.right || 0}}>
     {props.text}
   </ReactTooltip>
   </>
