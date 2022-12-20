6da6924917d7c1d7c84f37f1676fe6ef8800cc7a
ABDUL REHMAN <abdulrehman499949@gmail.com>
2022-12-21 03:29:38 +0500

    filters modal tooltip completed

--- src/pages/accounts/Expenses.jsx
+++ src/pages/accounts/Expenses.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Row, Col, Button } from "react-bootstrap";
+import { Row, Col, Button} from "react-bootstrap";
 import axios from "axios";
+import Form from 'react-bootstrap/Form';
 import EditButton from "../../components/UI/EditButton";
 import Table from "../../components/table/SmartTable";
 import MyModal from "../../components/modals/MyModal";
 import ExpenseModal from "../../components/expenseModal/ExpenseModal";
+import DeleteConfirmation from "../../components/modals/DeleteConfirmation";
+import TooltipCustom from "../../components/tooltip/TooltipCustom";
 import "../../assets/css/accounts/expenses.css"
 import { toast } from "react-toastify";
 
@@ -23,6 +26,44 @@ const Expenses = () => {
   const [showModal, setShowModal] = useState(false);
   const [defaultValue, setDefaultValue] = useState(null);
   const [rerenderTable, setRerenderTable] = useState(null);
+  const [year, setYear] = useState(new Date().getFullYear());
+  const [month, setMonth] = useState(new Date().getMonth());
+  const [deleteModal, setDeleteModal] = useState(false);
+  const [expenseId,setExpenseId] = useState();
+
+  const getYears = () => {
+    const YEAR = new Date().getFullYear();
+    const STARTING_YEAR = 2022;
+    const years = Array.from(
+      new Array(YEAR - STARTING_YEAR + 1),
+      (val, index) => index + STARTING_YEAR
+    );
+    return years;
+  };
+  const MONTHS = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const submitDeleteExpense = async () => {
+    deleteExpense(expenseId);
+    setDeleteModal(false)
+      }
+    const deleteExpenseBtn = (item)=>{
+      setDeleteModal(true)
+      setExpenseId(item)
+      }
+
   const createOption = (label) => ({
     label,
     value: label.toLowerCase().replace(/\W/g, ''),
@@ -47,7 +88,6 @@ const Expenses = () => {
     }
   }
   const renderBody = (item, index, currPage) =>  {
-    console.log(item,"items")
  return (
     <tr key={index}>
       <td>{index + 1 + currPage * PAGE_SIZE}</td>
@@ -56,16 +96,23 @@ const Expenses = () => {
       <td>{item.category}</td>
       <td>
       <div className="action_button_expenses_wrapper">
-        <div    className="action_btn_expenses_delete">
+      <TooltipCustom
+                          text="Delete Expense"
+                          id="expenseDelete"
+                        ></TooltipCustom>
+        <div     className="action_btn_expenses_delete" data-tip data-for="expenseDelete">
       <EditButton
-   
-                type="delete"
-                onClick={() => {
-                  deleteExpense(item._id);
-                }}
+                 type="delete"
+                 onClick={() => {
+                 deleteExpenseBtn(item._id)
+                  }}
               />
               </div>
-              <div  className="action_btn_expenses_edit"> 
+              <TooltipCustom
+                          text="Edit Expense"
+                          id="expenseEdit"
+                        ></TooltipCustom>
+              <div  className="action_btn_expenses_edit" data-tip data-for="expenseEdit"> 
         <EditButton
           type="edit"
           onClick={() => {
@@ -78,14 +125,47 @@ const Expenses = () => {
       </td>
     </tr>
   )};
-  var body = {};
+  var body = {
+    month: month,
+    year: year,
+  };
 
   return (
     <Row>
-      <Row className="m-3">
-        <Col md={3}></Col>
-        <Col md={5}></Col>
-        <Col md={4}>
+      <Row className="card-Heaing-Wrapper mb-2">
+        <Col md={2}>
+        <Form.Group controlId="formBasicSelect" className="years-Wrapper">
+        <Form.Label>Years</Form.Label>
+        <Form.Control
+          as="select"
+          value={year}
+          onChange={(e) =>{ setYear(e.target.value); setRerenderTable(Math.random())}}>
+              {getYears().map((y, i) => (
+              <option key={i} value={y}>
+                {y}
+              </option>
+            ))}
+        </Form.Control>
+      </Form.Group>
+        </Col>
+        <Col md={2}>
+        <Form.Group controlId="formBasicSelect">
+        <Form.Label>Month</Form.Label>
+        <Form.Control
+          as="select"
+          value={month}
+          
+          onChange={(e)=>{ setMonth(e.target.value); setRerenderTable(Math.random()) }}>
+            {MONTHS.map((y, i) => (
+              <option key={i} value={i}>
+                {y}
+              </option>
+            ))}
+        </Form.Control>
+      </Form.Group>
+        </Col>
+        <Col md={4}></Col>
+        <Col md={4} className="add-Expense-Btn">
             <Button
               onClick={() => {
                 setShowModal(true);
@@ -116,8 +196,6 @@ const Expenses = () => {
               renderBody(item, index, currPage)
             }
             total="Total"
-            // renderExportData={(data) => renderExportData(data)}
-            // exportData
           />
         </div>
       </div>
@@ -140,6 +218,13 @@ const Expenses = () => {
           }}
         />
       </MyModal>
+      <DeleteConfirmation
+                showModal={deleteModal}
+                confirmModal={submitDeleteExpense}
+                hideModal={() => setDeleteModal(false)}
+                message={"Are you Sure to want to delete Expense?"}
+                title="Delete Confirmation"
+              />
     </Row>
   );
 };
