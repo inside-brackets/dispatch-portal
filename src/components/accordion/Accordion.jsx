4a84ab378aed834d32d28ddb25555b36d07794c5
ABDUL REHMAN <abdulrehman499949@gmail.com>
2023-01-05 21:00:27 +0500

    worked on accordion

---
+++ src/components/accordion/Accordion.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './accordion.css'
+const Accordion = ({ title, content }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <div className="accordion-item">
+      <div className="accordion-title">
+        <div>{title}</div>
+        <div  onClick={() => setIsActive(!isActive)}>{isActive ? '-' : '+'}</div>
+      </div>
+      {isActive && <div className="accordion-content">{content}</div>}
+    </div>
+  );
+};
+
+export default Accordion;
